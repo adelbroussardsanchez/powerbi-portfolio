0640ea5ae131b34972330ba1998cfc62f774126f
Adel Broussard-Sanchez <adelbroussard@gmail.com>
2026-01-05 15:33:52 -0600

    Add files via upload

---
+++ dashboards/financial-packet-demo-a/Mcode/Balance-Sheet-Summary-Table/Equity Details Subtotal.m
@@ -0,0 +1,9 @@
+let
+    Source = #"Balance Sheet",
+    #"Merged Queries1" = Table.NestedJoin(Source, {"Account #"}, #"BS_Equity Accounts", {"Account #"}, "BS_Liability Accounts", JoinKind.LeftOuter),
+    #"Expanded BS_Liability Accounts" = Table.ExpandTableColumn(#"Merged Queries1", "BS_Liability Accounts", {"Level 1", "Level 1 Order", "Level 2 Conditional", "Level 2 Order", "Level 4", "Level 4 Conditional", "Level 4 Order"}, {"Level 1", "Level 1 Order", "Level 2 Conditional", "Level 2 Order", "Level 4", "Level 4 Conditional", "Level 4 Order"}),
+    #"Filtered Rows" = Table.SelectRows(#"Expanded BS_Liability Accounts", each ([Level 1] = "Equity")),
+    #"Grouped Rows" = Table.Group(#"Filtered Rows", {"Level 1", "Date"}, {{"Amount", each List.Sum([Amount]), type number}}),
+    #"Replaced Value" = Table.ReplaceValue(#"Grouped Rows","Equity","Total Equity",Replacer.ReplaceText,{"Level 1"})
+in
+    #"Replaced Value"
